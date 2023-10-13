6f0cf3174f684e145f4316feb056c031ae0900f8
nurealammiaji <nurealam.miaji@gmail.com>
2023-10-13 20:32:05 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Toy Store Server");
+})
+
+app.listen(port, () => {
+    console.log(`Toy Store Server is running on port: ${port}`);
+})
